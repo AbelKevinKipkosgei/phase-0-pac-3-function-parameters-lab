74b1ee535398a9942e06d1a8dbf07304fd97048c
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-06-25 12:27:11 +0300

    Made proper function calls.

---
+++ index.js
@@ -0,0 +1,17 @@
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
+}
+function introductionWithLanguage(name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+function introductionWithLanguageOptional(name, JavaScript) {
+  return `Hi, my name is ${name} and I am learning to program in JavaScript.`;
+}
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+introduction("Aki");
+introductionWithLanguage("Aki", "Ember.js");
+introductionWithLanguageOptional("Gracie");
+introductionWithLanguageOptional("Gracie", "Python");
